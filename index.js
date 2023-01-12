b3f6a5e1fa75d842f9b3972fb67dd3a8c7529ccf
ned <nedschultz@outlook.com>
2023-01-12 15:34:43 +1000

    commit tree fab9d37b3f64872f556ce50cf07e82ab1dd7ea04

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+const submitData = (name, email) => {
+    return fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+        .then((resp) => resp.json())
+        .then((obj) => {
+            let id = document.createElement("p");
+            id.textContent = `ID: ${obj.id}`;
+            document.body.appendChild(id);
+        })
+        .catch((err) => {
+            let errorMessage = document.createElement("p");
+            errorMessage.textContent = err.message;
+            document.body.appendChild(errorMessage);
+        });
+}
